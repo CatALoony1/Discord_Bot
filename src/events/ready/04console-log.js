2c3d0a1f7be89920d6677b3e355c2eb7603ab07a
Jonas <mc.huenchen321@gmail.com>
2024-12-29 13:42:19 +0100

    Richtiger Channel bei Nachricht bei start

--- src/events/ready/04console-log.js
+++ src/events/ready/04console-log.js
@@ -1,5 +1,5 @@
 module.exports = async (client) => {
     console.log(`Logged in as ${client.user.tag}!`);
-    var targetChannel = await client.channels.fetch(process.env.BUMP_ID);
+    var targetChannel = await client.channels.fetch(process.env.BOT_UPDATE_ID);
     targetChannel.send(`Bot ist gestartet.`);
 };
