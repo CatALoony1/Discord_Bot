047ad1e409f4d0a42167bafa46f337e6a3660f54
Jonas <mc.huenchen321@gmail.com>
2025-01-10 17:13:53 +0100

    Formatierung angepasst

--- src/events/messageCreate/giveUserXp.js
+++ src/events/messageCreate/giveUserXp.js
@@ -31,14 +31,14 @@ module.exports = async (message) => {
   if (!message.inGuild() || message.author.bot || cooldowns.has(message.author.id)) return;
 
   let multiplier = 1;
-let bonusWordList = [];
-for await (const doc of Config.find()){
- if(doc.key == "xpMultiplier"){
- multiplier = Number(doc.value);
-}else if(doc.key == "bonusWords"){
-bonusWordList = doc.value.split(',');
-}
-}
+  let bonusWordList = [];
+  for await (const doc of Config.find()) {
+    if (doc.key == "xpMultiplier") {
+      multiplier = Number(doc.value);
+    } else if (doc.key == "bonusWords") {
+      bonusWordList = doc.value.split(',');
+    }
+  }
 
   //BonusXP
   var bonusXP = 0;
@@ -58,11 +58,11 @@ bonusWordList = doc.value.split(',');
       bonusXP += getRandomXp(10, 20);
     }
   });
-  if(bonusXP > 0){
+  if (bonusXP > 0) {
     message.react("⭐");
     console.log(`user ${message.author.tag} received ${bonusXP} Bonus XP`);
   }
-  
+
   const xpToGive = (getRandomXp(5, 15) * multiplier) + bonusXP;
   const query = {
     userId: message.author.id,
