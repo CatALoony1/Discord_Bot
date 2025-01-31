9afc27ebd0303870ebedb179f948f604b8487163
Jonas <mc.huenchen321@gmail.com>
2025-01-31 13:57:09 +0100

    verschlafen eingebaut

--- src/events/ready/cronJob_goodMorning.js
+++ src/events/ready/cronJob_goodMorning.js
@@ -2,10 +2,25 @@ const Discord = require("discord.js");
 require('dotenv').config();
 const cron = require('node-cron');
 
+function getRandom(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 module.exports = async (client) => {
-  cron.schedule('0 7 * * *', async function () { // 7 Uhr
+  var verschlafen = cron.schedule('0 10 * * *', async function () { // 10 Uhr
     var targetChannel = await client.channels.fetch(process.env.MORNING_ID);
-    targetChannel.send(`Es ist 7 Uhr, raus aus den Kajüten und alle Mann an Deck!`);
+    targetChannel.send(`„Alle Mann an Deck! … Moment mal, warum ist es schon so hell?“`);
+    verschlafen.stop();
+  });
+  cron.schedule('0 7 * * *', async function () { // 7 Uhr
+    if (getRandom(1, 25) != 1) {
+      var targetChannel = await client.channels.fetch(process.env.MORNING_ID);
+      targetChannel.send(`Es ist 7 Uhr, raus aus den Kajüten und alle Mann an Deck!`);
+    } else {
+      verschlafen.start();
+    }
   });
 }
 
