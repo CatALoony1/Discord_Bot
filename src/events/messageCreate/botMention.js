f7dba85b46d15ab9d2b02e50d946e6609f8d24e5
Jonas <mc.huenchen321@gmail.com>
2025-01-31 15:37:19 +0100

    jetzt neu mit 30 Antwortmöglichkeiten

--- src/events/messageCreate/botMention.js
+++ src/events/messageCreate/botMention.js
@@ -15,7 +15,27 @@ const answers = new Map([[1, 'Ja!'],
 [7, 'Ist das nicht eindeutig?'],
 [8, 'Formuliere die Frage bitte klarer.'],
 [9, 'Ja, Ja und nochmal Ja!'],
-[10, 'Darüber sollte demokratisch abgestimmt werden.']
+[10, 'Darüber sollte demokratisch abgestimmt werden.'],
+[11, 'Ich denke nicht.'],
+[12, 'Ich werde darüber nachdenken.'],
+[13, 'Auf jeden Fall!'],
+[14, 'Nö :P'],
+[15, 'Yep!'],
+[16, 'Leider nicht. :('],
+[17, 'Ich fürchte du bist nicht berechtigt diese Antwort zu erfahren.'],
+[18, 'Eher nicht.'],
+[19, 'Wie bitte? Wurde ich etwas gefragt?'],
+[20, 'Ich bin leider im "Nein-Modus"!'],
+[21, 'Schlag seite 394 auf, dort steht die Antwort.'],
+[22, 'ERROR: Question to difficult!'],
+[23, 'Vielleicht ein anderes Map.'],
+[24, 'Unter anderen Umständen wäre das sicherlich denkbar.'],
+[25, 'Gewiss.'],
+[26, 'In der Tat.'],
+[27, 'Ja, so steht es in der Bibel geschrieben!'],
+[28, 'Ich bin noch unentschlossen.'],
+[29, 'Ich prüfe das...'],
+[30, 'Denk noch einmal genau über deine Frage nach.']
 ]);
 
 /**
@@ -26,5 +46,20 @@ const answers = new Map([[1, 'Ja!'],
 module.exports = async (message, client) => {
     console.log(`Bot Mentioned`);
     if (!message.inGuild() || message.author.bot || !message.mentions.has(client.user.id) || !message.content.includes("?")) return;
-    message.reply(answers.get(getRandom(1, 10)));
+    var number = getRandom(1, 30);
+    if(number == 22){
+        let sleep = async (ms) => await new Promise(r => setTimeout(r,ms));
+        var newMessage = await message.reply(answers.get(number));
+        newMessage = await newMessage.reply(`Self destruction initialized!`);
+        await sleep(1000);
+        newMessage = await newMessage.reply(`3`);
+        await sleep(1000);
+        newMessage = await newMessage.reply(`2`);
+        await sleep(1000);
+        newMessage = await newMessage.reply(`1`);
+        await sleep(1000);
+        newMessage = await newMessage.reply(`BOOM💥`);
+    }else{
+        await message.reply(answers.get(number));
+    }
 };
