06d5b5ff3c247bb315ca9aa542149b713392b7ec
Jonas <mc.huenchen321@gmail.com>
2025-02-11 12:45:10 +0100

    cronJob_checkInactive: Neue Nutzer 15 Tage hinzugefügt; Urlaubsmodus hinzugefügt

--- src/events/ready/cronJob_checkInactive.js
+++ src/events/ready/cronJob_checkInactive.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 require('dotenv').config();
 const cron = require('node-cron');
 const Level = require('../../models/Level');
+const Config = require('../../models/Config');
 
 module.exports = async (client) => {
     cron.schedule('0 1 * * *', async function () { // 1 Uhr
@@ -15,46 +16,68 @@ module.exports = async (client) => {
             console.log('ERROR: Niemand auf dem Server hat Level')
             return;
         }
-        var playersTags = [];
+        var away = [];
+        for await (const doc of Config.find()) {
+            if (doc.key == "away") {
+                away = doc.value.split(',');
+            }
+        }
+        var playerTags = [];
         for (let i = 0; i < fetchedLevel.length; i++) {
-            playersTags[i] = fetchedLevel[i].userName;
+            playerTags[i] = fetchedLevel[i].userName;
         }
         var playerTagsOnServer = [];
+        var playerTagsLurk = [];
         res.forEach((member) => {
-            for (let i = 0; i < playersTags.length; i++) {
-                if (playersTags[i] === member.user.tag) {
-                    let now = new Date();
-                    let diffTime = Math.abs(now - fetchedLevel[i].lastMessage);
-                    let diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
-                    if (diffDays < 30) { //User on Server
-                        playersTags[i] = 'good';//TODO check for rework
-                    } else {
-                        playerTagsOnServer[playerTagsOnServer.length] = playersTags[i];
-                    }
-                    break;
-                }
-            }
-        });
-        for (let i = 0; i < playersTags.length; i++) {
-            if (playersTags[i] != 'good') {
-                for (let j = 0; j < fetchedLevel.length; j++) {
-                    if (playersTags[i] === fetchedLevel[j].userName) {
+            if (!(away.length != 0 && away.includes(member.user.tag))) {
+                for (let i = 0; i < playerTags.length; i++) {
+                    if (playerTags[i] === member.user.tag) {
                         let now = new Date();
                         let diffTime = Math.abs(now - fetchedLevel[i].lastMessage);
                         let diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
-                        if (diffDays < 30) { //User not on Server
-                            playersTags[i] = 'good';
+                        if (diffDays < 30) { //User on Server
+                            playerTags[i] = 'good';
+                        } else {
+                            playerTagsOnServer[playerTagsOnServer.length] = playerTags[i];
                         }
                         break;
+                    }
+                    if (i == (playerTags.length - 1)) {
+                        let now = new Date();
+                        let diffTime = Math.abs(now - member.joinedAt());
+                        let diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+                        if (diffDays >= 15) { //User on Server, not DB
+                            playerTagsLurk[playerTagsLurk.length] = playerTags[i];
+                        }
+                    }
+                }
+            }
+        });
+        for (let i = 0; i < playerTags.length; i++) {
+            if (playerTags[i] != 'good') {
+                if (!(away.length != 0 && away.includes(playerTags[i]))) {
+                    for (let j = 0; j < fetchedLevel.length; j++) {
+                        if (playerTags[i] === fetchedLevel[j].userName) {
+                            let now = new Date();
+                            let diffTime = Math.abs(now - fetchedLevel[i].lastMessage);
+                            let diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+                            if (diffDays < 30) { //User not on Server
+                                playerTags[i] = 'good';
+                            }
+                            break;
+                        }
                     }
                 }
             }
         }
-        for (let i = 0; i < playersTags.length; i++) {
-            if (playersTags[i] != 'good') {
-                console.log(`User ${playersTags[i]} hasn't send a message in at least 30 Days.`);
-                await Level.deleteOne({ guildId: process.env.GUILD_ID, userName: playersTags[i], });
+        for (let i = 0; i < playerTags.length; i++) {
+            if (playerTags[i] != 'good') {
+                console.log(`User ${playerTags[i]} hasn't send a message in at least 30 Days.`);
+                await Level.deleteOne({ guildId: process.env.GUILD_ID, userName: playerTags[i], });
             }
+        }
+        for (let i = 0; i < playerTagsLurk.length; i++) {
+            console.log(`User ${playerTagsLurk[i]} hasn't send a message in at least 15 Days.`);
         }
 
         if (playerTagsOnServer.length != 0) {
@@ -68,6 +91,19 @@ module.exports = async (client) => {
             messageUserInactive.setTimestamp(Date.now());
             messageUserInactive.setTitle(`Folgende User haben seit 30 Tagen nichts geschrieben:`);
             messageUserInactive.setDescription(`${playerTagsOnServer.toString().replace(',', '\n')}`);
+            await targetChannel.send({ embeds: [messageUserInactive] });
+        }
+        if (playerTagsLurk.length != 0) {
+            const targetChannel = await client.channels.fetch(process.env.LOG_ID);
+            if (!targetChannel) {
+                console.log('Fehler, Logchannel gibts nicht');
+                return;
+            }
+            const messageUserInactive = new Discord.EmbedBuilder();
+            messageUserInactive.setColor(0xff0000);
+            messageUserInactive.setTimestamp(Date.now());
+            messageUserInactive.setTitle(`Seit 15 Tagen auf dem Server, nur am lurken`);
+            messageUserInactive.setDescription(`${playerTagsLurk.toString().replace(',', '\n')}`);
             await targetChannel.send({ embeds: [messageUserInactive] });
         }
         console.log(`CheckInactive-Job finished...`);
