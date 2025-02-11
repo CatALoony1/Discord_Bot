5491091ba718617806bb85351c7fc7eb67899827
Jonas <mc.huenchen321@gmail.com>
2025-02-11 13:50:57 +0100

    neue Befehle zum Aussetzen des Inaktivitäts-Check

---
+++ src/commands/add-informed-inactive.js
@@ -0,0 +1,45 @@
+const { SlashCommandBuilder, InteractionContextType, PermissionFlagsBits } = require('discord.js');
+const Config = require('../models/Config');
+
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('add-informed-inactive')
+        .setDescription('Setzt User beim Inaktivitäts-Check aus.')
+        .addMentionableOption(option =>
+            option.setName('nutzer')
+                .setDescription('Nutzer')
+                .setRequired(true)
+        )
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+        .setContexts([InteractionContextType.Guild, InteractionContextType.PrivateChannel]),
+
+    run: async ({ interaction, client }) => {
+        console.log(`SlashCommand ${interaction.commandName} was executed by user ${interaction.member.user.tag}`);
+        await interaction.deferReply({ flags: Discord.MessageFlags.Ephemeral });
+        if (!interaction.inGuild()) {
+            interaction.editReply('Hier ist doch kein Server!');
+            return;
+        }
+        const targetUserId = interaction.options.get('nutzer').value;
+        if (!(interaction.guild.members.cache.find(m => m.id === targetUserId)?.id)) {
+            interaction.editReply(`Bei ${targetUserId} handelt es sich nicht um einen Nutzer.`);
+            return;
+        }
+        const targetUserObj = await interaction.guild.members.fetch(targetUserId);
+        const config = await Config.findOne({
+            away: 'away',
+        });
+        if (config.value.includes(targetUserObj.user.tag)) {
+            await interaction.editReply(`Der User ${targetUserObj.user.tag} ist bereits einetragen.`);
+        } else {
+            if (config.value.length >= 1) {
+                config.value = `${config.value},${targetUserObj.user.tag}`;
+            } else {
+                config.value = `${targetUserObj.user.tag}`;
+            }
+            await config.save();
+            await interaction.editReply(`Der User ${targetUserObj.user.tag} wurde einetragen.`);
+        }
+    },
+};
---
+++ src/commands/remove-informed-inactive.js
@@ -0,0 +1,43 @@
+const { SlashCommandBuilder, InteractionContextType, PermissionFlagsBits } = require('discord.js');
+const Config = require('../models/Config');
+
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('remove-informed-inactive')
+        .setDescription('Aktiviere User beim Inaktivitäts-Check.')
+        .addMentionableOption(option =>
+            option.setName('nutzer')
+                .setDescription('Nutzer')
+                .setRequired(true)
+        )
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+        .setContexts([InteractionContextType.Guild, InteractionContextType.PrivateChannel]),
+
+    run: async ({ interaction, client }) => {
+        console.log(`SlashCommand ${interaction.commandName} was executed by user ${interaction.member.user.tag}`);
+        await interaction.deferReply({ flags: Discord.MessageFlags.Ephemeral });
+        if (!interaction.inGuild()) {
+            interaction.editReply('Hier ist doch kein Server!');
+            return;
+        }
+        const targetUserId = interaction.options.get('nutzer').value;
+        if (!(interaction.guild.members.cache.find(m => m.id === targetUserId)?.id)) {
+            interaction.editReply(`Bei ${targetUserId} handelt es sich nicht um einen Nutzer.`);
+            return;
+        }
+        const targetUserObj = await interaction.guild.members.fetch(targetUserId);
+        const config = await Config.findOne({
+            away: 'away',
+        });
+        if (config.value.includes(targetUserObj.user.tag)) {
+            let away = config.value.split(',');
+            away = away.splice(away.indexOf(targetUserObj.user.tag), 1);
+            config.value = away.toString();
+            await config.save();
+            await interaction.editReply(`Der User ${targetUserObj.user.tag} wurde entfernt.`);
+        } else {
+            await interaction.editReply(`Der User ${targetUserObj.user.tag} ist gar nicht einetragen.`);
+        }
+    },
+};
