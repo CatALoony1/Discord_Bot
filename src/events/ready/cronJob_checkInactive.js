02dd896baf9d1430eb541828f58b5c8daa2d34ee
Jonas <mc.huenchen321@gmail.com>
2025-02-12 14:10:55 +0100

    checkInactive now deletes QuizStats

--- src/events/ready/cronJob_checkInactive.js
+++ src/events/ready/cronJob_checkInactive.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const cron = require('node-cron');
 const Level = require('../../models/Level');
 const Config = require('../../models/Config');
+const QuizStats = require('../../models/QuizStats');
 
 module.exports = async (client) => {
     cron.schedule('0 1 * * *', async function () { // 1 Uhr
@@ -23,48 +24,48 @@ module.exports = async (client) => {
                     away = doc.value.split(',');
                 }
             }
-            var playerTags = [];
+            var playerTags = new Map();
             for (let i = 0; i < fetchedLevel.length; i++) {
-                playerTags[i] = fetchedLevel[i].userName;
+                playerTags.set(fetchedLevel[i].userName, fetchedLevel[i].userId);
             }
             var playerTagsOnServer = [];
-            var playerTagsLurk = [];
+            var playerTagsLurk = new Map();
             members.forEach(async (member) => {
                 if (!(away.length != 0 && away.includes(member.user.tag)) && !member.user.bot) {
-                    for (let i = 0; i < playerTags.length; i++) {
-                        if (playerTags[i] === member.user.tag) {
+                    for (let i = 0; i < playerTags.keys().length; i++) {
+                        if (playerTags.keys()[i] === member.user.tag) {
                             let now = new Date();
                             let diffTime = Math.abs(now - fetchedLevel[i].lastMessage);
                             let diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
                             if (diffDays < 30) { //User on Server
-                                playerTags[i] = 'good';
+                                playerTags.keys()[i] = 'good';
                             } else {
-                                playerTagsOnServer[playerTagsOnServer.length] = playerTags[i];
+                                playerTagsOnServer[playerTagsOnServer.length] = member.user.tag;
                             }
                             break;
                         }
-                        if (i == (playerTags.length - 1)) {
+                        if (i == (playerTags.keys().length - 1)) {
                             let now = new Date();
                             let joinDate = member.joinedAt;
                             let diffTime = Math.abs(now - joinDate);
                             let diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
                             if (diffDays >= 15) { //User on Server, not DB
-                                playerTagsLurk[playerTagsLurk.length] = member.user.tag;
+                                playerTagsLurk.set(member.user.tag, member.user.id);
                             }
                         }
                     }
                 }
             });
-            for (let i = 0; i < playerTags.length; i++) {
-                if (playerTags[i] != 'good') {
-                    if (!(away.length != 0 && away.includes(playerTags[i]))) {
+            for (let i = 0; i < playerTags.keys().length; i++) {
+                if (playerTags.keys()[i] != 'good') {
+                    if (!(away.length != 0 && away.includes(playerTags.keys()[i]))) {
                         for (let j = 0; j < fetchedLevel.length; j++) {
-                            if (playerTags[i] === fetchedLevel[j].userName) {
+                            if (playerTags.keys()[i] === fetchedLevel[j].userName) {
                                 let now = new Date();
                                 let diffTime = Math.abs(now - fetchedLevel[i].lastMessage);
                                 let diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
                                 if (diffDays < 30) { //User not on Server
-                                    playerTags[i] = 'good';
+                                    playerTags.keys()[i] = 'good';
                                 }
                                 break;
                             }
@@ -72,14 +73,27 @@ module.exports = async (client) => {
                     }
                 }
             }
-            for (let i = 0; i < playerTags.length; i++) {
-                if (playerTags[i] != 'good') {
-                    await console.log(`User ${playerTags[i]} hasn't send a message in at least 30 Days.`);
-                    await Level.deleteOne({ guildId: process.env.GUILD_ID, userName: playerTags[i], });
+            const fetchedQuizstats = await QuizStats.find({
+                guildId: process.env.GUILD_ID,
+            });
+            var quizUserIds = [];
+            for(let stat of fetchedQuizstats){
+                quizUserIds[quizUserIds.length] = stat.userId;
+            }
+            for (let i = 0; i < playerTags.keys().length; i++) {
+                if (playerTags.keys()[i] != 'good') {
+                    await console.log(`User ${playerTags.keys()[i]} hasn't send a message in at least 30 Days.`);
+                    await Level.deleteOne({ guildId: process.env.GUILD_ID, userName: playerTags.keys()[i], });
+                    if(quizUserIds.includes(playerTags.get(playerTags.keys[i]))){
+                        await QuizStats.deleteOne({guildId: process.env.GUILD_ID, userId: playerTags.get(playerTags.keys[i]),});
+                    }
                 }
             }
-            for (let i = 0; i < playerTagsLurk.length; i++) {
-                await console.log(`User ${playerTagsLurk[i]} hasn't send a message in at least 15 Days.`);
+            for (let i = 0; i < playerTagsLurk.keys().length; i++) {
+                await console.log(`User ${playerTagsLurk.keys()[i]} hasn't send a message in at least 15 Days.`);
+                if(quizUserIds.includes(playerTagsLurk.get(playerTagsLurk.keys[i]))){
+                    await QuizStats.deleteOne({guildId: process.env.GUILD_ID, userId: playerTagsLurk.get(playerTagsLurk.keys[i]),});
+                }
             }
 
             if (playerTagsOnServer.length != 0) {
