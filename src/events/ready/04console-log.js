85deb55d0571e4a19ad7cce057fb62f2e4647231
Jonas <mc.huenchen321@gmail.com>
2025-03-02 06:15:35 +0100

    hilfe?

--- src/events/ready/04console-log.js
+++ src/events/ready/04console-log.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 module.exports = async (client) => {
     console.log(`Logged in as ${client.user.tag}!`);
-    const targetUser = await client.guild.members.fetch(process.env.ADMIN_ID);
+    const guild = await client.guilds.cache.get(process.env.GUILD_ID);
+    const targetUser = await guild.members.fetch(process.env.ADMIN_ID);
     targetUser.send(`Bot ist gestartet.`);
 };
