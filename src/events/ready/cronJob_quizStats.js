e0a4a024c152bab1dd75f5e7869d72e11e5b97d4
Jonas <mc.huenchen321@gmail.com>
2025-03-09 12:51:39 +0100

    fix Quizstats

--- src/events/ready/cronJob_quizStats.js
+++ src/events/ready/cronJob_quizStats.js
@@ -5,38 +5,38 @@ const QuizQuestions = require('../../models/QuizQuestion')
 
 module.exports = async (client) => {
     cron.schedule('11 1 * * 7', async function () {
-        try{
-var targetChannel = await client.channels.fetch(process.env.QUIZ_ID);
-        const embed = await createQuizLeaderboardEmbeds(0, client);
-        const pageDownButton = new ButtonBuilder()
-            .setEmoji('⬅️')
-            .setLabel('Zurück')
-            .setStyle(ButtonStyle.Primary)
-            .setCustomId(`qPageDown`);
+        try {
+            var targetChannel = await client.channels.fetch(process.env.QUIZ_ID);
+            const embed = await createQuizLeaderboardEmbeds(0, client);
+            const pageDownButton = new ButtonBuilder()
+                .setEmoji('⬅️')
+                .setLabel('Zurück')
+                .setStyle(ButtonStyle.Primary)
+                .setCustomId(`qPageDown`);
 
-        const pageUpButton = new ButtonBuilder()
-            .setEmoji('➡️')
-            .setLabel('Vorwärts')
-            .setStyle(ButtonStyle.Primary)
-            .setCustomId(`qPageUp`);
+            const pageUpButton = new ButtonBuilder()
+                .setEmoji('➡️')
+                .setLabel('Vorwärts')
+                .setStyle(ButtonStyle.Primary)
+                .setCustomId(`qPageUp`);
 
-        const firstRow = new ActionRowBuilder().addComponents(pageDownButton, pageUpButton);
+            const firstRow = new ActionRowBuilder().addComponents(pageDownButton, pageUpButton);
 
-        await targetChannel.send({
-            embeds: [embed],
-            components: [firstRow]
-        })
+            await targetChannel.send({
+                embeds: [embed],
+                components: [firstRow]
+            })
 
-        const fetchedQuestions = await QuizQuestions.find({
-            asked: 'N',
-        });
-        const numberQuestions = new EmbedBuilder();
-        numberQuestions.setColor(0x868686);
-        numberQuestions.setTitle(`Anzahl der Fragen in der DB:`);
-        numberQuestions.setDescription(`${fetchedQuestions.length}`);
-        await targetChannel.send({ embeds: [numberQuestions] });
-} catch(error) {
-console.log(error);
-}
+            const fetchedQuestions = await QuizQuestions.find({
+                asked: 'N',
+            });
+            const numberQuestions = new EmbedBuilder();
+            numberQuestions.setColor(0x868686);
+            numberQuestions.setTitle(`Anzahl der Fragen in der DB:`);
+            numberQuestions.setDescription(`${fetchedQuestions.length}`);
+            await targetChannel.send({ embeds: [numberQuestions] });
+        } catch (error) {
+            console.log(error);
+        }
     });
 };
