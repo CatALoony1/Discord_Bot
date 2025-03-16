232ab50c657632500f92afe89b814306a9f0636b
Jonas <mc.huenchen321@gmail.com>
2025-03-16 19:19:21 +0100

    vllt so

--- src/events/messageCreate/adminMention.js
+++ src/events/messageCreate/adminMention.js
@@ -29,7 +29,8 @@ module.exports = async (message, client) => {
         const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
         model.systemInstruction = 'Dein Name ist Jonas, du befindest dich auf einem Discord Server. Deine Antworten sind kurz und knapp. Du antwortest ausschließlich auf SFW und jugendfreie Fragen.';
         console.log(prompt);
-        const result = await model.generateContent([prompt]);
+        const result = await model.generateContent(String(prompt));
+        //const result = await model.generateContent([prompt]);
         console.log(result.response.text());
         await webhookClient.send(`Da Jonas aktuell keine Lust hat, werde ich antworten:\n\n${result.response.text()}\n\n\n||Diese Antwort entspricht zu 100% der Meinung und ist definitiv nicht KI-generiert. Vielleicht lüge ich aber auch.||`);
     } catch (error) {
