769e224ba2536cdbb3617fb0c20991565b127460
Jonas <51858363+CatALoony1@users.noreply.github.com>
2025-03-17 08:40:13 +0100

    Aktualisieren von schmutz.js

--- src/commands/schmutz.js
+++ src/commands/schmutz.js
@@ -1,6 +1,5 @@
 const { SlashCommandBuilder, MessageFlags } = require('discord.js');
 require('dotenv').config();
-const { GoogleGenerativeAI } = require("@google/generative-ai");
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('schmutz')
@@ -22,19 +21,14 @@ module.exports = {
             await interaction.deferReply({ flags: MessageFlags.Ephemeral });
             const id = interaction.options.get('messageid').value;
             const fetchedMessage = await interaction.channel.messages.fetch(id);
-            //await fetchedMessage.react('🇸');
-           // await fetchedMessage.react('🇨');
-         //   await fetchedMessage.react('🇭');
-          //  await fetchedMessage.react('🇲');
-       //     await fetchedMessage.react('🇺');
-       //     await fetchedMessage.react('🇹');
-     //       await fetchedMessage.react('🇿');
-const prompt = fetchedMessage.content;
-const genAI = new GoogleGenerativeAI(process.env.AI_API);
-const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
-const result = await model.generateContent(String(prompt)); 
-console.log(result.response.text());
-          await interaction.editReply('Erledigt!');
+            await fetchedMessage.react('🇸');
+            await fetchedMessage.react('🇨');
+            await fetchedMessage.react('🇭');
+            await fetchedMessage.react('🇲');
+            await fetchedMessage.react('🇺');
+            await fetchedMessage.react('🇹');
+            await fetchedMessage.react('🇿');
+            await interaction.editReply('Erledigt!');
         } catch (err) {
             console.log(err);
         }
