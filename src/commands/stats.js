7e25822355d6bfb6b85b5d7440bcf037f9181493
Jonas <mc.huenchen321@gmail.com>
2025-03-22 19:12:09 +0100

    lotto stats

--- src/commands/stats.js
+++ src/commands/stats.js
@@ -68,6 +68,9 @@ module.exports = {
       guildId: interaction.guild.id,
       userId: targetUserId,
     });
+    console.log(lotto);
+    console.log(lotto.length);
+    console.log(lotto.size);
     var lottospiele = 0;
     if (lotto && lotto.length > 0) {
     }
