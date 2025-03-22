2253ba45c241b6c44eef7b64ce9a582f21e1f616
Jonas <mc.huenchen321@gmail.com>
2025-03-22 12:05:14 +0100

    AI is stupid

--- src/events/messageCreate/botMention.js
+++ src/events/messageCreate/botMention.js
@@ -76,7 +76,7 @@ module.exports = async (message, client) => {
         state = newBotstate;
     }
     if (botstatevar == 'neutral') {
-        var number = getRandom(1, 31);
+        var number = getRandom(1, 40);
         var delay = 2000;
         if (number == 22) {
             let sleep = async (ms) => await new Promise(r => setTimeout(r, ms));
