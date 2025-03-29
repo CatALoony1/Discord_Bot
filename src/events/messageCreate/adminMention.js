d23a1a651a16b292ee81882cc515aa1ab949d8e0
Jonas <mc.huenchen321@gmail.com>
2025-03-29 11:16:50 +0100

    Mention KI Jonas Role

--- src/events/messageCreate/adminMention.js
+++ src/events/messageCreate/adminMention.js
@@ -10,7 +10,7 @@ const getAIResult = require('../../utils/getAIResult');
  * @returns 
  */
 module.exports = async (message) => {
-    if (!message.inGuild() || message.author.bot || !message.content.includes(process.env.ADMIN_ID) || message.webhookId || message.channel.id != process.env.WELCOME_ID) return;
+    if (!message.inGuild() || message.author.bot || !message.content.includes(process.env.KI_JONAS) || message.webhookId || message.channel.id != process.env.WELCOME_ID) return;
     console.log(`Admin Mentioned`);
     try {
         const config = await Config.findOne({
@@ -23,7 +23,7 @@ module.exports = async (message) => {
         });
         if (!begruessung) return;
         let webhookClient = new WebhookClient({ id: begruessung.webhookId, token: begruessung.webhookToken });
-        const prompt = message.content.replace(`<@${process.env.ADMIN_ID}>`, 'Jonas');
+        const prompt = message.content.replace(`<@&${process.env.KI_JONAS}>`, 'Jonas');
         const sysInstruction = "Dein Name ist Jonas, du befindest dich auf einem Discord Server und hast aktuell keine Lust auf die anderen. Deine Antworten sind kurz und knapp.";
         const result = await getAIResult(prompt, sysInstruction);
         await webhookClient.send(`Da Jonas aktuell keine Lust hat, werde ich antworten:\n${result.response.text()}\n||Diese Antwort entspricht zu 100% meiner Meinung und ist definitiv nicht KI-generiert. Vielleicht lüge ich aber auch.||`);
