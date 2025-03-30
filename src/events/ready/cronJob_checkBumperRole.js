9bc51f92f7836df0b2da3b4860194ead39d3f770
Jonas <mc.huenchen321@gmail.com>
2025-03-30 10:37:59 +0200

    Fix error

--- src/events/ready/cronJob_checkBumperRole.js
+++ src/events/ready/cronJob_checkBumperRole.js
@@ -3,30 +3,28 @@ const cron = require('node-cron');
 const Level = require('../../models/Level');
 
 module.exports = async (client) => {
-    cron.schedule('*/6 * * * *', async function () {
+    cron.schedule('*/5 * * * *', async function () {
         try {
             const guild = client.guilds.cache.get(process.env.GUILD_ID);
-            let members = await guild.members.fetch();
-            members.forEach(async (member) => {
-                const query = {
-                    userId: member.user.id,
-                    guildId: guild.id,
-                };
-                const level = await Level.findOne(query);
-                if (level) {
-                    if (member.roles.cache.some(role => role.name === 'Bumper')) {
-                        let now = new Date();
-                        let lastbump = level.lastBump;
-                        let diffTime = Math.abs(now - lastbump);
-                        let diffHour = Math.floor(diffTime / (1000 * 60 * 60));
-                        if (diffHour >= 24) {
+            const allLevels = await Level.find({
+                guildId: process.env.GUILD_ID,
+            });
+            for (const level of allLevels) {
+                if (level.lastBump) {
+                    let now = new Date();
+                    let lastbump = level.lastBump;
+                    let diffTime = Math.abs(now - lastbump);
+                    let diffHour = Math.floor(diffTime / (1000 * 60 * 60));
+                    if (diffHour == 24) {
+                        let targetUserObj = await guild.members.cache.get(level.userId);
+                        if (targetUserObj.roles.cache.some(role => role.name === 'Bumper')) {
                             let tempRole = guild.roles.cache.find(role => role.name === 'Bumper');
-                            await member.roles.remove(tempRole);
+                            await targetUserObj.roles.remove(tempRole);
                             console.log(`Role Bumper was removed from user ${member.user.tag}`);
                         }
                     }
                 }
-            });
+            }
         } catch (error) {
             console.log(error);
         }
