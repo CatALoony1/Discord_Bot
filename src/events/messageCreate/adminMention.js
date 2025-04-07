a8665f6976cc707995b7b4afacb3f9a9c25ff777
Jonas <mc.huenchen321@gmail.com>
2025-04-07 10:30:42 +0200

    KI-Jonas

--- src/events/messageCreate/adminMention.js
+++ src/events/messageCreate/adminMention.js
@@ -34,7 +34,7 @@ module.exports = async (message) => {
         let webhookClient = new WebhookClient({ id: begruessung.webhookId, token: begruessung.webhookToken });
         const prompt = message.content.replace(`<@&${process.env.KI_JONAS}>`, 'Jonas');
         //const sysInstruction = "Dein Name ist Jonas, du befindest dich auf einem Discord Server und hast aktuell keine Lust auf die anderen. Deine Antworten sind kurz und knapp.";
-        const sysInstruction = "Dein Name ist Jonas und du befindest dich auf einem Discord Server. Du Antwortest mit maximal einem Satz und du antwortest kokett.";
+        const sysInstruction = "Dein Name ist Jonas, du befindest dich auf einem Discord Server und bist sehr kokett. Du Antwortest mit maximal einem Satz.";
         const result = await getAIResult(prompt, sysInstruction);
         await webhookClient.send(`Da Jonas aktuell keine Lust hat, werde ich antworten:\n${result.response.text()}\n||Diese Antwort entspricht zu 100% meiner Meinung und ist definitiv nicht KI-generiert. Vielleicht lüge ich aber auch.||`);
     } catch (error) {
