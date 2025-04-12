f0ae5ec44d86c51786bc3d1b758b64ab131d871d
Jonas <mc.huenchen321@gmail.com>
2025-04-12 10:00:10 +0200

    combine Select Menus

---
+++ src/events/interactionCreate/selectButton.js
@@ -0,0 +1,85 @@
+const { MessageFlags } = require('discord.js');
+
+const {
+    ageRoles,
+    colorRoles,
+    dmRoles,
+    gameRoles,
+    pingRoles,
+    platformRoles,
+    pronounRoles,
+    regionRoles
+} = require('../../utils/selectMenuRoles');
+
+module.exports = async (interaction) => {
+    if (interaction.isButton() && interaction.customId && interaction.customId.includes('remove')) {
+        await interaction.deferReply({ flags: MessageFlags.Ephemeral });
+        let customId = interaction.customId;
+        let variant = 0;
+        let roleArray = [];
+        switch (customId) {
+            case 'removeAge':
+                variant = 1;
+                roleArray = ageRoles;
+                break;
+            case 'removeColor':
+                variant = 1;
+                roleArray = colorRoles;
+                break;
+            case 'removeDm':
+                variant = 1;
+                roleArray = dmRoles;
+                break;
+            case 'removeGames':
+                variant = 2;
+                roleArray = gameRoles;
+                break;
+            case 'removePingRoles':
+                variant = 2;
+                roleArray = pingRoles;
+                break;
+            case 'removePlatformRoles':
+                variant = 2;
+                roleArray = platformRoles;
+                break;
+            case 'removePronouns':
+                variant = 2;
+                roleArray = pronounRoles;
+                break;
+            case 'removeRegion':
+                variant = 1;
+                roleArray = regionRoles;
+                break;
+            default:
+                console.log('Unknown customId');
+                return;
+        }
+        if (variant == 1) {
+            for (let i = 0; i < roleArray.length; i++) {
+                if (interaction.member.roles.cache.some(role => role.name === roleArray[i])) {
+                    let tempRole = interaction.guild.roles.cache.find(role => role.name === roleArray[i]);
+                    await interaction.guild.members.cache.get(interaction.member.id).roles.remove(tempRole);
+                    console.log(`Role ${roleArray[i]} was removed from user ${interaction.member.user.tag}`);
+                    await interaction.editReply(`Die Rolle ${roleArray[i]} wurde dir entzogen.`);
+                    return;
+                }
+            }
+            await interaction.editReply(`Du hattest gar keine Altersrolle.`);
+        } else if (variant == 2) {
+            let removedRoles = [];
+            for (let i = 0; i < roleArray.length; i++) {
+                if (interaction.member.roles.cache.some(role => role.name === roleArray[i])) {
+                    let tempRole = interaction.guild.roles.cache.find(role => role.name === roleArray[i]);
+                    await interaction.guild.members.cache.get(interaction.member.id).roles.remove(tempRole);
+                    console.log(`Role ${roleArray[i]} was removed from user ${interaction.member.user.tag}`);
+                    removedRoles[removedRoles.length] = roleArray[i];
+                }
+            }
+            if (removedRoles.length != 0) {
+                await interaction.editReply(`Die Rollen ${removedRoles} wurde entfernt.`);
+            } else {
+                await interaction.editReply(`Du hattest gar keine Rollen dieses Menüs.`);
+            }
+        }
+    }
+};
---
+++ src/utils/selectMenuRoles.js
@@ -0,0 +1,85 @@
+const ageRoles = ['<18',
+    '18-21',
+    '22-25',
+    '26-29',
+    'Ü 30'];
+
+const colorRoles = ['Hering Silber',
+    'Thunfisch Grau',
+    'Ozean-Türkis',
+    'Kapitän Blau',
+    'Lagunenblau',
+    'Sturmbraus-Blau',
+    'Marineblau',
+    'Lachsrosa',
+    'Krabbenrot',
+    'Leuchtturm-Rot',
+    'Fischernetz-Rostrot',
+    'Voll Korall',
+    'Zitronengelb',
+    'Goldbarsch Gelb',
+    'Panadegold',
+    'Dillgrün',
+    'Seegras-Grün',
+    'Algen-Grün',
+    'Tiefsee-Algen-Grün'];
+
+const dmRoles = ['DMs open',
+    'DMs closed'];
+
+const dmEmojis = ['🔓', '🔒'];
+
+const gameRoles = ['League of Legends',
+    'Minecraft',
+    'Dead by Daylight',
+    'Fortnite',
+    'Overwatch',
+    'Call of Duty',
+    'Garry\'s Mod',
+    'Rocket League'];
+
+const pingRoles = ['Begrüßungskomitee',
+    'Bump-Ping',
+    'Event-Ping'];
+
+const pingEmojis = ['👋', '👊', '📅'];
+
+const platformRoles = ['XBOX',
+    'Switch',
+    'PC',
+    'Playstation',
+    'Mobile'];
+
+const pronounRoles = ['He/him',
+    'She/her',
+    'They/them'];
+
+const regionRoles = ['Niedersachsen',
+    'Bayern',
+    'Berlin',
+    'Hessem',
+    'Thüringen',
+    'Bremen',
+    'Baden-Württemberg',
+    'Saarland',
+    'Sachsen',
+    'Sachsen-Anhalt',
+    'Mecklenburg-Vorpommern',
+    'Brandenburg',
+    'Schleswig-Holstein',
+    'Nordrhein-Westfalen',
+    'Hamburg',
+    'Rheinland-Pfalz'];
+
+module.exports = {
+    ageRoles,
+    colorRoles,
+    dmRoles,
+    dmEmojis,
+    gameRoles,
+    pingRoles,
+    pingEmojis,
+    platformRoles,
+    pronounRoles,
+    regionRoles
+};
