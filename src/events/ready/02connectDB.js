c47ad71d5453bba9d78019a91148f903fb98bc04
Jonas <mc.huenchen321@gmail.com>
2025-04-29 08:52:10 +0200

    mongoose logging

--- src/events/ready/02connectDB.js
+++ src/events/ready/02connectDB.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 module.exports = async () => {
   try {
     mongoose.set('strictQuery', false);
+    mongoose.set("debug", (collectionName, method, query, doc) => {
+      console.log(`Mongoose: ${collectionName}.${method}`, JSON.stringify(query), doc);
+    });
     await mongoose.connect(process.env.MONGODB_URI);
     console.log('Connected to DB.');
   } catch (err) {
