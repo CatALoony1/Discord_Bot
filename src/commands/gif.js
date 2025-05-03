ac04e08d9ff8da14f7aeb3b6a8b2fe3a642d93db
Jonas <51858363+CatALoony1@users.noreply.github.com>
2025-05-03 20:16:02 +0200

    Aktualisieren von gif.js

--- src/commands/gif.js
+++ src/commands/gif.js
@@ -16,7 +16,7 @@ module.exports = {
         console.log(`SlashCommand ${interaction.commandName} was executed by user ${interaction.member.user.tag}`);
         await interaction.deferReply();
         const suchwort = interaction.options.get('suchwort').value;
-        const regex = /^[\u0041-\u005A\u00C4\u00D6\u00DC\u00DF\s]+$/i; //A-Z, ÄÖÜ, ß
+        const regex = /^[\u0041-\u005A\u0061-\u007A\u00C4\u00D6\u00DC\u00E4\u00F6\u00FC\u00DF\s]+$/; // A-Z, a-z, ÄÖÜäöü, ß
         if (!regex.test(suchwort)) {
             await interaction.deferReply({ flags: MessageFlags.Ephemeral });
             await interaction.editReply('Das übergebene Wort enthält Zeichen die nicht zugelassen sind.');
