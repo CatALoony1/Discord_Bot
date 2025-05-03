7e288770f34573a9c304f1e982b51b94b16c2f64
Jonas <mc.huenchen321@gmail.com>
2025-05-03 17:44:35 +0200

    hangman comman

---
+++ src/models/Hangman.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose');
+const { randomUUID } = require('crypto');
+
+const hangmanSchema = new Schema({
+  authorId: {
+    type: String,
+    required: true,
+  },
+  guildId: {
+    type: String,
+    required: true,
+  },
+  messageId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  word: {
+    type: String,
+    required: true,
+  },
+  status: {
+    //laufend, beendet
+    type: String,
+    default: "laufend",
+  },
+  buchstaben: {
+    type: [String],
+    default: [],
+  },
+  fehler: {
+    type: Number,
+    default: 0,
+  },
+});
+
+module.exports = model('Hangman', hangmanSchema);
