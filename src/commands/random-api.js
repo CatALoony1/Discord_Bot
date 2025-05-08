ccd86c24e01458c6b72b331b54ca698a888a83f9
CatALoony1 <huhn.gudrun31@gmail.com>
2025-05-08 12:19:04 +0200

    random api

--- src/commands/random-api.js
+++ src/commands/random-api.js
@@ -131,7 +131,7 @@ module.exports = {
                     break;
                 case 12:
                     const marvelParameter = generiereMarvelHash();
-                    const apiUrl = `https://gateway.marvel.com/v1/public/characters?apikey=${process.env.MARVEL_PUBLIC}`;
+                    const apiUrl = `https://gateway.marvel.com/v1/public/characters?ts=${marvelParameter.ts}&apikey=${process.env.MARVEL_PUBLIC}&hash=${marvelParameter.hash}`;
                     await fetch(apiUrl)
                         .then((response) => response.json())
                         .then((mydata) => {
