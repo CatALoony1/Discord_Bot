7bc4f6452cb7fee7720e05e52f3b735043c4cb44
CatALoony1 <huhn.gudrun31@gmail.com>
2025-05-08 07:46:39 +0200

    fix

--- src/utils/getAIResult.js
+++ src/utils/getAIResult.js
@@ -4,11 +4,14 @@ async function getAIResult(prompt, sysInstruction) {
     console.log('contacting AI');
     let result = null;
     try {
-        const genAI = new GoogleGenAI(process.env.AI_API);
+        const genAI = new GoogleGenAI({ apiKey: process.env.AI_API });
         let model = null;
+        let response = null;
+        console.log(`AI-Input:${String(prompt)}`);
         if ((prompt.toLowerCase().includes('erzeuge') || prompt.toLowerCase().includes('erstelle') || prompt.toLowerCase().includes('generiere')) && prompt.toLowerCase().includes('bild')) {
-            model = genAI.getGenerativeModel({
+            response = await ai.models.generateContent({
                 model: "gemini-2.0-flash-preview-image-generation",
+                contents: String(prompt),
                 systemInstruction: sysInstruction,
                 config: {
                     maxOutputTokens: 250,
@@ -24,15 +27,22 @@ async function getAIResult(prompt, sysInstruction) {
                     maxOutputTokens: 250,
                 },
             });
+            response = await ai.models.generateContent({
+                model: "gemini-2.0-flash",
+                contents: String(prompt),
+                systemInstruction: sysInstruction,
+                config: {
+                    tools: [{ googleSearch: {} }],
+                    maxOutputTokens: 250,
+                },
+            });
         }
-        console.log(`AI-Input:${String(prompt)}`);
-        result = await model.generateContent(String(prompt));
-        console.log(`AI-Result:${result.response.text()}`);
-        console.log(result.response.usageMetadata.totalTokenCount);
+        console.log(`AI-Result:${response.text()}`);
+        console.log(response.usageMetadata.totalTokenCount);
     } catch (error) {
         console.log(error);
     }
-    return result;
+    return response;
 }
 
 module.exports = getAIResult;
