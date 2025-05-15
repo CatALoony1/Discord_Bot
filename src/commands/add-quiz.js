437de3da73f7d1a2e3a753b73cc65d520185e4eb
Jonas <51858363+CatALoony1@users.noreply.github.com>
2025-05-15 16:42:04 +0200

    Aktualisieren von add-quiz.js

--- src/commands/add-quiz.js
+++ src/commands/add-quiz.js
@@ -20,31 +20,31 @@ module.exports = {
                 .setLabel('Frage:')
                 .setStyle(TextInputStyle.Paragraph)
                 .setRequired(true)
-                .setMaxLength(350);
+                .setMaxLength(300);
             const richtigInput = new TextInputBuilder()
                 .setCustomId('qaddbyadmin-richtig')
                 .setLabel('Richtige Antwort:')
                 .setStyle(TextInputStyle.Paragraph)
                 .setRequired(true)
-                .setMaxLength(150);
+                .setMaxLength(100);
             const falsch1Input = new TextInputBuilder()
                 .setCustomId('qaddbyadmin-falsch1')
                 .setLabel('Falsche Antwort 1:')
                 .setStyle(TextInputStyle.Paragraph)
                 .setRequired(true)
-                .setMaxLength(150);
+                .setMaxLength(100);
             const falsch2Input = new TextInputBuilder()
                 .setCustomId('qaddbyadmin-falsch2')
                 .setLabel('Falsche Antwort 2:')
                 .setStyle(TextInputStyle.Paragraph)
                 .setRequired(true)
-                .setMaxLength(150);
+                .setMaxLength(100);
             const falsch3Input = new TextInputBuilder()
                 .setCustomId('qaddbyadmin-falsch3')
                 .setLabel('Falsche Antwort 3:')
                 .setStyle(TextInputStyle.Paragraph)
                 .setRequired(true)
-                .setMaxLength(150);
+                .setMaxLength(100);
             const firstActionRow = new ActionRowBuilder().addComponents(frageInput);
             const secondActionRow = new ActionRowBuilder().addComponents(richtigInput);
             const thirdctionRow = new ActionRowBuilder().addComponents(falsch1Input);
