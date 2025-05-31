4b623548d3ee6223a8b8b27e086da62e5dd04a10
CatALoony1 <mc.huenchen321@gmail.com>
2025-05-31 10:15:52 +0200

    neuer befehl /geschenk

---
+++ src/commands/geschenk.js
@@ -0,0 +1,61 @@
+const { SlashCommandBuilder, InteractionContextType } = require('discord.js');
+const removeXP = require('../utils/removeXP');
+const giveXP = require('../utils/giveXP');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('geschenk')
+        .setDescription('Verschenke XP an einen Nutzer (sie werden von dir abgezogen).')
+        .addMentionableOption(option =>
+            option.setName('nutzer')
+                .setDescription('Nutzer dem du XP schenken willst.')
+                .setRequired(true)
+        )
+        .addIntegerOption(option =>
+            option.setName('xpmenge')
+                .setDescription('Die Menge an XP die der Nutzer von dir erhalten soll.')
+                .setRequired(true)
+                .setMinValue(1)
+        )
+        .addStringOption(option =>
+            option.setName('nachricht')
+                .setDescription('(optional)Hänge eine Nachricht an das Geschenk.')
+                .setRequired(false)
+                .setMinLength(1)
+        )
+        .setContexts([InteractionContextType.Guild, InteractionContextType.PrivateChannel]),
+
+    run: async ({ interaction }) => {
+        console.log(`SlashCommand ${interaction.commandName} was executed by user ${interaction.member.user.tag}`);
+        try {
+            if (!interaction.inGuild()) {
+                interaction.reply('Hier ist doch kein Server!');
+                return;
+            }
+            await interaction.deferReply();
+            if (subcommand == 'multiplier') {
+                const targetUserId = interaction.options.get('nutzer').value;
+                if (!(interaction.guild.members.cache.find(m => m.id === targetUserId)?.id)) {
+                    interaction.editReply(`Bei ${targetUserId} handelt es sich nicht um einen Nutzer.`);
+                    return;
+                }
+                if (interaction.user.id === targetUserId) {
+                    interaction.editReply('Du kannst dir selbst keine XP schenken!');
+                    return;
+                }
+                const targetUserObj = await interaction.guild.members.fetch(targetUserId);
+                let xpAmount = interaction.options.get('xpmenge').value;
+                const reason = interaction.options.get('nachricht')?.value || "";
+                await removeXP(interaction.member, xpAmount, interaction.channel);
+                await giveXP(targetUserObj, xpAmount, xpAmount, interaction.channel, false, false, false);
+                if (reason !== "") {
+                    await interaction.editReply(`${targetUserObj} du hast ${xpAmount}XP von ${interaction.member} erhalten!\nAngehängte Nachricht:\n${reason}`);
+                } else {
+                    await interaction.editReply(`${targetUserObj} du hast ${xpAmount}XP von ${interaction.member} erhalten!`);
+                }
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    },
+};
