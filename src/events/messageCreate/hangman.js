fb0368d04050412140b9f134b361efe09872b4c4
CatALoony1 <mc.huenchen321@gmail.com>
2025-06-01 11:12:07 +0200

    hangman fix

--- src/events/messageCreate/hangman.js
+++ src/events/messageCreate/hangman.js
@@ -17,10 +17,10 @@ const giveXP = require("../../utils/giveXP");
 }*/
 
 function maskiereWort(wort, gerateneBuchstaben) {
-    const woerter = wort.split('');
+    const woerter = wort.split(' ');
     const maskierteWoerter = woerter.map(einzelWort => {
         return einzelWort.split('').map(buchstabe => {
-            if (gerateneBuchstaben.includes(buchstabe.toLowerCase())) {
+            if (gerateneBuchstaben.includes(buchstabe)) {
                 return buchstabe;
             } else {
                 return '\\_';
