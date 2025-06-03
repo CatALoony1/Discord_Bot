84dc3bad6f4f8d19e6b3e5145ce7680b9a96b5e9
Jonas <51858363+CatALoony1@users.noreply.github.com>
2025-06-03 22:42:36 +0200

    Aktualisieren von getAIResult.js

--- src/utils/getAIResult.js
+++ src/utils/getAIResult.js
@@ -7,13 +7,13 @@ async function getAIResult(prompt, sysInstruction) {
         const genAI = new GoogleGenAI({ apiKey: process.env.AI_API });
         console.log(`AI-Input:${String(prompt)}`);
         response = await genAI.models.generateContent({
-            //model: "gemini-2.0-flash",
-            model: "gemini-2.5-flash-preview-05-20",
+            model: "gemini-2.0-flash",
+            //model: "gemini-2.5-flash-preview-05-20",
             contents: String(prompt),
             config: {
                 systemInstruction: sysInstruction,
                 tools: [{ googleSearch: {} }],
-                maxOutputTokens: 250,
+                maxOutputTokens: 500,
             },
         });
         console.log(`AI-Result:${response.text}`);
