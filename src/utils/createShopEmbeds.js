efbbaadbf5201fc1645f9c371089412fefb9e0f3
CatALoony1 <mc.huenchen321@gmail.com>
2025-06-04 18:35:21 +0200

    spiele shop buttons

--- src/utils/createShopEmbeds.js
+++ src/utils/createShopEmbeds.js
@@ -6,10 +6,9 @@ async function createLeaderboardEmbeds(page, interaction) {
     const booster = interaction.member.roles.cache.some(role => role.name === 'Server Booster') ? true : false;
     const allItems = await Items.find({boostOnly: booster});
 
-  var i = page + 1;
   const embed = new EmbedBuilder()
     .setTitle(`Shop - ${page + 1}/${allItems.length}`)
-    .setDescription(`Name:${allItems[i].name}\nPreis: ${allItems[i].preis} Loserlinge\nBeschreibung: ${allItems[i].beschreibung}`)
+    .setDescription(`Name:${allItems[page].name}\nPreis: ${allItems[page].preis} Loserlinge\nBeschreibung: ${allItems[page].beschreibung}`)
     .setColor(0x0033cc);
   return embed;
 }
