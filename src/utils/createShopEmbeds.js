d5bb74895d29670beb92e1223e1f001ec29d0b3f
CatALoony1 <mc.huenchen321@gmail.com>
2025-06-04 18:44:36 +0200

    spiele shop booster fix

--- src/utils/createShopEmbeds.js
+++ src/utils/createShopEmbeds.js
@@ -4,13 +4,18 @@ const Items = require('../models/Items');
 
 async function createLeaderboardEmbeds(page, interaction) {
     const booster = interaction.member.roles.cache.some(role => role.name === 'Server Booster') ? true : false;
-    const allItems = await Items.find({boostOnly: booster});
+    let allItems = [];
+    if (booster) {
+        allItems = await Items.find({});
+    } else {
+        allItems = await Items.find({ boostOnly: booster });
+    }
 
-  const embed = new EmbedBuilder()
-    .setTitle(`Shop - ${page + 1}/${allItems.length}`)
-    .setDescription(`Name:${allItems[page].name}\nPreis: ${allItems[page].preis} Loserlinge\nBeschreibung: ${allItems[page].beschreibung}`)
-    .setColor(0x0033cc);
-  return embed;
+    const embed = new EmbedBuilder()
+        .setTitle(`Shop - ${page + 1}/${allItems.length}`)
+        .setDescription(`Name:${allItems[page].name}\nPreis: ${allItems[page].preis} Loserlinge\nBeschreibung: ${allItems[page].beschreibung}`)
+        .setColor(0x0033cc);
+    return embed;
 }
 
 module.exports = createLeaderboardEmbeds;
