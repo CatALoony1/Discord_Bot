1c9f6859e97f92319f55a5ff45c1ce514ffacb89
CatALoony1 <mc.huenchen321@gmail.com>
2025-06-05 12:42:12 +0200

    spiele shop kaufen

--- src/events/interactionCreate/shopButtons.js
+++ src/events/interactionCreate/shopButtons.js
@@ -2,6 +2,8 @@ const { MessageFlags } = require('discord.js');
 const createShopEmbeds = require("../../utils/createShopEmbeds");
 const GameUser = require('../../models/GameUser');
 require('../../models/Bankkonten');
+require('../../models/Inventar');
+const Items = require('../../models/Items');
 
 module.exports = async (interaction) => {
     if (!interaction.isButton() || !interaction.customId || !interaction.customId.includes('shop')) return;
@@ -45,16 +47,36 @@ module.exports = async (interaction) => {
         try {
             const description = targetMessageEmbed.description;
             const itemName = description.substring(description.indexOf('Name:') + 6, description.indexOf('\n'));
-            const price = parseInt(description.substring(description.indexOf('Preis:') + 7, description.indexOf('Loserlinge') -1));
+            const price = parseInt(description.substring(description.indexOf('Preis:') + 7, description.indexOf('Loserlinge') - 1));
             console.log(`Item: ${itemName}, Price: ${price}`);
-            const user = await GameUser.findOne({ userId: interaction.user.id }).populate('bankkonto');
-            if (!user || !user.bankkonto) {
+            const user = await GameUser.findOne({ userId: interaction.user.id }).populate('bankkonto').populate('inventar');
+            if (!user || !user.bankkonto || !user.inventar) {
                 await interaction.reply({ content: 'Du hast kein Bankkonto!', flags: MessageFlags.Ephemeral });
                 return;
             }
             if (user.bankkonto.currentMoney < price) {
                 await interaction.reply({ content: 'Du hast nicht genug Geld auf deinem Bankkonto!', flags: MessageFlags.Ephemeral });
                 return;
+            }
+            //Check if User already has Item in user.inventar.items
+            if (user.inventar.items.includes(itemName)) {
+                await interaction.reply({ content: `Du hast bereits ein ${itemName} in deinem Inventar!`, flags: MessageFlags.Ephemeral });
+                return;
+            }
+            const item = await Items.findOne({ name: itemName });
+            if (!item) {
+                await interaction.reply({ content: `Das Item ${itemName} existiert nicht!`, flags: MessageFlags.Ephemeral });
+                return;
+            }
+            const itemIndex = user.inventar.items.findIndex(inventarItem => inventarItem.name === itemName);
+            if (itemIndex !== -1) {
+                user.inventar.items[itemIndex].quantity += 1;
+                await user.inventar.save();
+                await interaction.reply({ content: `Du hast ein ${itemName} gekauft!`, flags: MessageFlags.Ephemeral });
+            } else {
+                user.inventar.items.push({ item: item._id, quantity: 1 });
+                await user.inventar.save();
+                await interaction.reply({ content: `Du hast ein ${itemName} gekauft!`, flags: MessageFlags.Ephemeral });
             }
             return;
         } catch (error) {
