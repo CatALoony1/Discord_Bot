b5dc20d731e29bf1e18850c2fc8b92956827fc1f
CatALoony1 <mc.huenchen321@gmail.com>
2025-06-07 14:08:20 +0200

    remove free for admin

--- src/events/interactionCreate/shopButtons.js
+++ src/events/interactionCreate/shopButtons.js
@@ -56,7 +56,7 @@ module.exports = async (interaction) => {
                 await interaction.reply({ content: 'Du hast kein Bankkonto!', flags: MessageFlags.Ephemeral });
                 return;
             }
-            if (user.bankkonto.currentMoney < price && interaction.user.id != process.env.ADMIN_ID && interaction.user.id != '582571514474266635') {
+            if (user.bankkonto.currentMoney < price) {
                 await interaction.reply({ content: 'Du hast nicht genug Geld auf deinem Bankkonto!', flags: MessageFlags.Ephemeral });
                 return;
             }
@@ -75,9 +75,7 @@ module.exports = async (interaction) => {
                 await user.inventar.save();
                 await interaction.reply({ content: `Du hast ein ${itemName} gekauft!`, flags: MessageFlags.Ephemeral });
             }
-            if (interaction.user.id != process.env.ADMIN_ID && interaction.user.id != '582571514474266635') {
-                await removeMoney(interaction.member, price);
-            }
+            await removeMoney(interaction.member, price);
             return;
         } catch (error) {
             console.log(error);
