c6eaac85ad30c0ef7045663d7f4e33336ad37f6d
Jonas <51858363+CatALoony1@users.noreply.github.com>
2025-06-08 08:50:25 +0200

    Aktualisieren von giveUserXp.js

--- src/events/messageCreate/giveUserXp.js
+++ src/events/messageCreate/giveUserXp.js
@@ -18,38 +18,13 @@ function getRandomXp(min, max) {
 module.exports = async (message) => {
   if (!message.inGuild() || message.author.bot || cooldowns.has(message.author.id) || message.webhookId) return;
   let multiplier = 1;
-  let bonusWordList = [];
   for await (const doc of Config.find({ guildId: message.guild.id })) {
     if (doc.key == "xpMultiplier") {
       multiplier = Number(doc.value);
-    } else if (doc.key == "bonusWords") {
-      bonusWordList = doc.value.split(',');
     }
-  }
-  //BonusXP
-  var bonusXP = 0;
-  const messageLength = message.content.length;
-  if (messageLength == 69) {
-    bonusXP += 69;
-  } else if (messageLength == 666) {
-    bonusXP += 666;
-  } else if (messageLength >= 1500) {
-    bonusXP += 200;
-    if (messageLength >= 3000) {
-      bonusXP += 500;
-    }
-  }
-  bonusWordList.forEach(word => {
-    if (message.content.toLowerCase().includes(word)) {
-      bonusXP += getRandomXp(10, 20);
-    }
-  });
-  if (bonusXP > 0) {
-    message.react("⭐");
-    console.log(`user ${message.author.tag} received ${bonusXP} Bonus XP`);
   }
   var xpToGive = (getRandomXp(5, 15) * multiplier) + bonusXP;
-  await giveXP(message.member, xpToGive, bonusXP, message.channel, true, false);
+  await giveXP(message.member, xpToGive, 0, message.channel, true, false);
   cooldowns.add(message.author.id);
   setTimeout(() => {
     cooldowns.delete(message.author.id);
