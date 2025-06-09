db81e429d6ca0bb128f096155df038aec0743fa9
CatALoony1 <mc.huenchen321@gmail.com>
2025-06-09 07:40:50 +0200

    remove role on join

--- src/events/guildMemberAdd/addRole.js
+++ src/events/guildMemberAdd/addRole.js
@@ -2,6 +2,4 @@ require('dotenv').config();
 module.exports = async (guildMember) => {
     const role = guildMember.guild.roles.cache.get(process.env.NEWMEMBER_ROLE_ID);
     await guildMember.guild.members.cache.get(guildMember.id).roles.add(role);
-    const roleSpiele = guildMember.guild.roles.cache.get('1379457961931309137'); //Spielkind
-    await guildMember.guild.members.cache.get(guildMember.id).roles.add(roleSpiele);
 };
