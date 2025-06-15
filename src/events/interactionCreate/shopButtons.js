47a6d76076674c6aba9b813e5192e5a7f03f89bd
CatALoony1 <mc.huenchen321@gmail.com>
2025-06-15 07:53:02 +0200

    improve shop

--- src/events/interactionCreate/shopButtons.js
+++ src/events/interactionCreate/shopButtons.js
@@ -12,36 +12,38 @@ module.exports = async (interaction) => {
     let targetMessage = await interaction.channel.messages.fetch(interaction.message.id);
     let targetMessageEmbed = targetMessage.embeds[0];
     let [, pageSlash] = targetMessageEmbed.title.split(" - ");
-    let [page, maxpage] = pageSlash.split("/");
+    let [currentPage, maxPage] = pageSlash.split("/");
+    currentPage = parseInt(currentPage);
+    maxPage = parseInt(maxPage);
     if (interaction.customId === 'shopDown') {
         try {
-            if (page != 1) {
-                let newPage = +page;
-                await interaction.update({
-                    embeds: [await createShopEmbeds(newPage - 2, interaction)],
-                    components: [targetMessage.components[0], targetMessage.components[1]]
-                });
-                return;
+            let newPage;
+            if (currentPage === 1) {
+                newPage = maxPage;
             } else {
-                await interaction.reply({ content: `Du bist bereits auf Seite 1.`, flags: MessageFlags.Ephemeral });
-                return;
+                newPage = currentPage - 1;
             }
+            await interaction.update({
+                embeds: [await createShopEmbeds(newPage - 1, interaction)],
+                components: [targetMessage.components[0], targetMessage.components[1]]
+            });
+            return;
         } catch (error) {
             console.log(error);
         }
     } else if (interaction.customId === 'shopUp') {
         try {
-            if (page != maxpage) {
-                let newPage = +page;
-                await interaction.update({
-                    embeds: [await createShopEmbeds(newPage, interaction)],
-                    components: [targetMessage.components[0], targetMessage.components[1]]
-                });
-                return;
+            let newPage;
+            if (currentPage === maxPage) {
+                newPage = 1;
             } else {
-                await interaction.reply({ content: `Du bist bereits auf der letzten Seite.`, flags: MessageFlags.Ephemeral });
-                return;
+                newPage = currentPage + 1;
             }
+            await interaction.update({
+                embeds: [await createShopEmbeds(newPage - 1, interaction)],
+                components: [targetMessage.components[0], targetMessage.components[1]]
+            });
+            return;
         } catch (error) {
             console.log(error);
         }
