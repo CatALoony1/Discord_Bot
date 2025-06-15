69747a69410fe85050ce197cec9a687663a76e82
CatALoony1 <mc.huenchen321@gmail.com>
2025-06-15 15:15:15 +0200

    owoify

---
+++ src/commands/owoify.js
@@ -0,0 +1,36 @@
+const { ApplicationCommandType } = require("discord.js");
+
+module.exports = {
+    data: {
+        name: 'owoify',
+        type: ApplicationCommandType.Message,
+    },
+    run: async ({ interaction }) => {
+        try {
+            interaction.deferReply();
+            const content = interaction.targetMessage.content;
+            let reply = '';
+            const fetch = await import('node-fetch').then(module => module.default);
+            const apiUrl = 'https://nekos.life/api/v2/owoify';
+            const response = await fetch(apiUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ text: content })
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            const data = await response.json();
+            if (data.owo) {
+                await interaction.editReply(data.owo);
+            } else {
+                await interaction.editReply('Dies hat nicht funktioniert!');
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    },
+};
