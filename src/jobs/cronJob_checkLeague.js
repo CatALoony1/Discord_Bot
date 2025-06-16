4ff1200fb1847f7f71899d369c7231d0f4977ff9
CatALoony1 <mc.huenchen321@gmail.com>
2025-06-16 10:01:07 +0200

    league job

--- src/jobs/cronJob_checkLeague.js
+++ src/jobs/cronJob_checkLeague.js
@@ -31,18 +31,36 @@ function isRunning() {
 }
 
 async function jobFunction(client) {
-    var targetChannel = await client.channels.fetch(process.env.LOG_ID);
-    const guild = await client.guilds.cache.get(process.env.GUILD_ID);
-    const fetch = await import('node-fetch').then(module => module.default);
-    let data;
-    const fetchMatchURL = `https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/${process.env.KIRA_L_PUUID}/ids?start=0&count=1&api_key=${process.env.LEAGUE_API}`;
-    console.log(fetchMatchURL);
-    await fetch(fetchMatchURL)
-        .then((response) => response.json())
-        .then((mydata) => {
-            data = mydata;
-        });
-    console.log(data);
+    try {
+        var targetChannel = await client.channels.fetch(process.env.LOG_ID);
+        const guild = await client.guilds.cache.get(process.env.GUILD_ID);
+        const fetch = await import('node-fetch').then(module => module.default);
+        let matchID;
+        const fetchMatchesURL = `https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/${process.env.KIRA_L_PUUID}/ids?start=0&count=1&api_key=${process.env.LEAGUE_API}`;
+        await fetch(fetchMatchesURL)
+            .then((response) => response.json())
+            .then((mydata) => {
+                matchID = mydata;
+            });
+        console.log(matchID[0]);
+        let matchData;
+        const fetchMatchURL = `https://europe.api.riotgames.com/lol/match/v5/matches/${matchID[0]}?api_key=${process.env.LEAGUE_API}`;
+        await fetch(fetchMatchesURL)
+            .then((response) => response.json())
+            .then((mydata) => {
+                matchData = mydata;
+            });
+        console.log(matchData);
+        let userMatchData;
+        for (const user of matchData.info.participants) {
+            if (user.puuid && user.puuid == process.env.KIRA_L_PUUID) {
+                userMatchData = user;
+            }
+        }
+        console.log(userMatchData);
+    } catch (e) {
+        console.log(e);
+    }
 }
 
 module.exports = {
