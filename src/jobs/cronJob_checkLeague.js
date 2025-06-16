5b7e5b72b013bc7745542268c92abde15e9b7ac9
CatALoony1 <mc.huenchen321@gmail.com>
2025-06-16 09:52:42 +0200

    league job

--- src/jobs/cronJob_checkLeague.js
+++ src/jobs/cronJob_checkLeague.js
@@ -34,6 +34,7 @@ async function jobFunction(client) {
     var targetChannel = await client.channels.fetch(process.env.LOG_ID);
     const guild = await client.guilds.cache.get(process.env.GUILD_ID);
     const fetch = await import('node-fetch').then(module => module.default);
+    let data;
     const fetchMatchURL = `https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/${process.env.KIRA_L_PUUID}/ids?start=0&count=1&api_key=${process.env.LEAGUE_API}`;
     await fetch(fetchMatchURL)
         .then((response) => response.json())
