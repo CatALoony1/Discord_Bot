0fa63728d8653aebe8002719627005b2c7775936
CatALoony1 <mc.huenchen321@gmail.com>
2025-06-24 19:13:13 +0200

    fix

--- src/events/messageCreate/aiMention.js
+++ src/events/messageCreate/aiMention.js
@@ -12,7 +12,7 @@ module.exports = async (message) => {
     console.log(`KI Mentioned`);
     try {
         const prompt = message.content.replaceAll(`<@&${process.env.KI_BOT}>`, 'Yamcha');
-        let sysInstruction = `Dein Name ist ${person} und du befindest dich auf einem Discord Server namens 'LEAFing Reality'. Da Discord eine Zeichengrenze bei Nachrichten hat, antwortest du mit unter 1500 Zeichen.`;
+        let sysInstruction = `Dein Name ist Yamcha und du befindest dich auf einem Discord Server namens 'LEAFing Reality'. Da Discord eine Zeichengrenze bei Nachrichten hat, antwortest du mit unter 1500 Zeichen.`;
         sysInstruction = `${sysInstruction} Du bist hilfsbereit und freundlich. Du antwortest jederzeit nach bestem Wissen und Gewissen.`;
         let response = await getAIResult(`Nachricht von ${message.author.displayName}: ${prompt}`, sysInstruction);
         await message.reply(`${response.text}\n||KI-Generierter Text!||`);
