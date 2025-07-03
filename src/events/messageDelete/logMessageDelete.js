d3ec8f6bd3dfe4357b91e2adbb137c609ef4db02
CatALoony1 <mc.huenchen321@gmail.com>
2025-07-03 20:19:59 +0200

    logging for hangman

--- src/events/messageDelete/logMessageDelete.js
+++ src/events/messageDelete/logMessageDelete.js
@@ -8,7 +8,9 @@ const { EmbedBuilder, Message, AuditLogEvent, Client } = require('discord.js');
  */
 module.exports = async (message, client) => {
   console.log(`message deleted in ${message.channel}`);
-  if (message.author != null && (message.author.bot || message.webhookId) && (message.channel.id === process.env.SPIELE_ID && message.content.length == 1)) return;
+  if (message.author != null && (message.author.bot || message.webhookId) && (message.channel.id === process.env.SPIELE_ID && message.reference && message.content.length == 1)) return;
+  console.log(message.content);
+  console.log(message.content.length);
   try {
     const targetChannel = message.guild.channels.cache.get(process.env.LOG_ID) || (await message.guild.channels.fetch(process.env.LOG_ID));
     if (!targetChannel) {
