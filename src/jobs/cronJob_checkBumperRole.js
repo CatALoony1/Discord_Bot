68c1a1de10d3852fd813e5449d7eb216fe23a992
CatALoony1 <mc.huenchen321@gmail.com>
2025-08-12 15:40:57 +0200

    fix

--- src/jobs/cronJob_checkBumperRole.js
+++ src/jobs/cronJob_checkBumperRole.js
@@ -14,18 +14,20 @@ function startJob(client) {
             const { levelDAO } = getDaos();
             const guild = client.guilds.cache.get(process.env.GUILD_ID);
             const allLevels = await levelDAO.getAllByGuild(process.env.GUILD_ID);
-            for (const level of allLevels) {
-                if (level.lastBump) {
-                    let now = new Date();
-                    let lastbump = level.lastBump;
-                    let diffTime = Math.abs(now - lastbump);
-                    let diffHour = Math.floor(diffTime / (1000 * 60 * 60));
-                    if (diffHour == 24) {
-                        let targetUserObj = await guild.members.cache.get(level.userId);
-                        if (targetUserObj.roles.cache.some(role => role.name === 'Bumper')) {
-                            let tempRole = guild.roles.cache.find(role => role.name === 'Bumper');
-                            await targetUserObj.roles.remove(tempRole);
-                            console.log(`Role Bumper was removed from user ${level.userName}`);
+            if (allLevels && allLevels.length > 0) {
+                for (const level of allLevels) {
+                    if (level.lastBump) {
+                        let now = new Date();
+                        let lastbump = level.lastBump;
+                        let diffTime = Math.abs(now - lastbump);
+                        let diffHour = Math.floor(diffTime / (1000 * 60 * 60));
+                        if (diffHour == 24) {
+                            let targetUserObj = await guild.members.cache.get(level.userId);
+                            if (targetUserObj.roles.cache.some(role => role.name === 'Bumper')) {
+                                let tempRole = guild.roles.cache.find(role => role.name === 'Bumper');
+                                await targetUserObj.roles.remove(tempRole);
+                                console.log(`Role Bumper was removed from user ${level.userName}`);
+                            }
                         }
                     }
                 }
