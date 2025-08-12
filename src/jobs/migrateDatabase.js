d3fb25e4f810739d60321d5f2b7d1f25c156ddd5
CatALoony1 <mc.huenchen321@gmail.com>
2025-08-12 15:07:14 +0200

    fix

--- src/jobs/migrateDatabase.js
+++ src/jobs/migrateDatabase.js
@@ -35,80 +35,12 @@ async function jobFunction(client) {
         const { activeItemsDAO, bankkontenDAO, bumpDAO, configDAO, gameUserDAO, gluecksradDAO, hangmanDAO, inventarDAO, itemsDAO, levelDAO, lottozahlenDAO, quizQuestionDAO, quizStatsDAO, tiereDAO } = getDaos();
         const backupDone = await Config.findOne({ key: 'BackupDone' });
         if (backupDone.value == 'N') {
-            console.log('Start Migrating Items');
-            const allItems = await Items.find({});
-            console.log(allItems);
-            if (allItems && allItems.length > 0) {
-                let itemsToSave = [];
-                for (let item of allItems) {
-                    console.log(item);
-                    itemsToSave.push(new SQLItems(item._id.toString(), item.name, item.beschreibung, item.preis, item.boostOnly, item.available));
-                }
-                console.log(`Saving ${itemsToSave.length} of ${allItems.length} Items.`);
-                console.log(itemsToSave);
-                let amount = await itemsDAO.insertMany(itemsToSave);
-                console.log(`${amount} of ${itemsToSave.length} Items saved.`);
-            }
-            console.log('Finished Migrating Items');
-
-            console.log('Start Migrating GameUser');
-            const allGameUser = await GameUser.find({});
-            if (allGameUser && allGameUser.length > 0) {
-                let itemsToSave = [];
-                for (let item of allGameUser) {
-                    itemsToSave.push(new SQLGameUser(item._id.toString(), item.userId, item.guildId, item.quizadded, item.daily, item.weight));
-                }
-                console.log(`Saving ${itemsToSave.length} of ${allGameUser.length} GameUser.`);
-                let amount = await gameUserDAO.insertMany(itemsToSave);
-                console.log(`${amount} of ${itemsToSave.length} GameUser saved.`);
-            }
-            console.log('Finished Migrating GameUser');
-
-            console.log('Start Migrating ActiveItems');
-            const allActiveItems = await ActiveItems.find({});
-            if (allActiveItems && allActiveItems.length > 0) {
-                let itemsToSave = [];
-                for (let item of allActiveItems) {
-                    itemsToSave.push(new SQLActiveItems(item._id.toString(), item.guildId, item.endTime, item.itemType, item.user, item.usedOn, item.extras));
-                }
-                console.log(`Saving ${itemsToSave.length} of ${allActiveItems.length} ActiveItems.`);
-                let amount = await activeItemsDAO.insertMany(itemsToSave);
-                console.log(`${amount} of ${itemsToSave.length} ActiveItems saved.`);
-            }
-            console.log('Finished Migrating ActiveItems');
-
-            console.log('Start Migrating Bump');
-            const allBump = await Bump.find({});
-            if (allBump && allBump.length > 0) {
-                let itemsToSave = [];
-                for (let item of allBump) {
-                    itemsToSave.push(new SQLBump(item._id.toString(), item.guildId, item.endTime, item.reminded, item.remindedId));
-                }
-                console.log(`Saving ${itemsToSave.length} of ${allBump.length} Bump.`);
-                let amount = await bumpDAO.insertMany(itemsToSave);
-                console.log(`${amount} of ${itemsToSave.length} Bump saved.`);
-            }
-            console.log('Finished Migrating Bump');
-
-            console.log('Start Migrating Config');
-            const allConfigs = await Config.find({});
-            if (allConfigs && allConfigs.length > 0) {
-                let itemsToSave = [];
-                for (let item of allConfigs) {
-                    itemsToSave.push(new SQLConfig(item._id.toString(), item.guildId, item.key, item.value));
-                }
-                console.log(`Saving ${itemsToSave.length} of ${allConfigs.length} Config.`);
-                let amount = await configDAO.insertMany(itemsToSave);
-                console.log(`${amount} of ${itemsToSave.length} Config saved.`);
-            }
-            console.log('Finished Migrating Config');
-
             console.log('Start Migrating Bankkonten');
             const allBankkonten = await Bankkonten.find({});
             if (allBankkonten && allBankkonten.length > 0) {
                 let itemsToSave = [];
                 for (let item of allBankkonten) {
-                    itemsToSave.push(new SQLBankkonten(item._id.toString(), item.currentMoney, item.moneyGain, item.moneyLost, item.zinsProzent, item.besitzer));
+                    itemsToSave.push(new SQLBankkonten(item._id.toString(), item.currentMoney, item.moneyGain, item.moneyLost, item.zinsProzent, item.besitzer.toString()));
                 }
                 console.log(`Saving ${itemsToSave.length} of ${allBankkonten.length} Bankkonten.`);
                 let amount = await bankkontenDAO.insertMany(itemsToSave);
@@ -151,7 +83,7 @@ async function jobFunction(client) {
                     for (let itemObj of Inventar.items) {
                         allItems.push({ quantity: itemObj.amount, itemId: itemObj.item._id.toString() });
                     }
-                    itemsToSave.push(new SQLInventar(item._id.toString(), allItems, item.besitzer));
+                    itemsToSave.push(new SQLInventar(item._id.toString(), allItems, item.besitzer.toString()));
                 }
                 console.log(`Saving ${itemsToSave.length} of ${allInventar.length} Inventar.`);
                 let amount = await inventarDAO.insertMany(itemsToSave);
@@ -216,7 +148,7 @@ async function jobFunction(client) {
             if (alltiere && alltiere.length > 0) {
                 let itemsToSave = [];
                 for (let item of alltiere) {
-                    itemsToSave.push(new SQLTiere(item._id.toString(), item.pfad, item.tierart, item.customName, item.besitzer));
+                    itemsToSave.push(new SQLTiere(item._id.toString(), item.pfad, item.tierart, item.customName, item.besitzer.toString()));
                 }
                 console.log(`Saving ${itemsToSave.length} of ${alltiere.length} Tiere.`);
                 let amount = await tiereDAO.insertMany(itemsToSave);
