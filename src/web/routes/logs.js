93296790516cf9d4bdf019da47d476d54b5477c4
CatALoony_ <mc.huenchen321@gmail.com>
2026-07-21 08:49:52 +0200

    fix

--- src/web/routes/logs.js
+++ src/web/routes/logs.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const path = require('path');
+const express = require('express');
+const router = express.Router();
 
-app.get('/logs', requireLogin, (req, res) => {
+router.get('/logs', requireLogin, (req, res) => {
   const logPath = path.join(__dirname, '../../../logs/bot._log');
   let logData = 'Keine Log-Datei gefunden.';
   if (fs.existsSync(logPath)) {
