410a984365f89aba389365c09fa1313eda458a79
CatALoony_ <mc.huenchen321@gmail.com>
2026-07-21 09:15:19 +0200

    kleine anpassungen

--- src/web/routes/logs.js
+++ src/web/routes/logs.js
@@ -4,6 +4,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
+  if (req.session.guildIds !== 'all') {
+    req.session.message = 'Du bist dazu nicht berechtigt!';
+    return res.redirect('/');
+  }
   const logPath = path.join(__dirname, '../../../logs/bot._log');
   let logData = 'Keine Log-Datei gefunden.';
   if (fs.existsSync(logPath)) {
