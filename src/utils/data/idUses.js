c5cf5b144f24da73dc2c73484271dec70f953fe0
CatALoony_ <mc.huenchen321@gmail.com>
2026-07-23 14:51:15 +0200

    Verhindern der Anzeige nicht erlaubter Tabellen und idUses

---
+++ src/utils/data/idUses.js
@@ -0,0 +1,23 @@
+const idUses = {
+  ALLGEMEIN_ID:
+    'Wird verwendet für Willkommens-, Boost-, Neujahrs- & Geburtstagsnachrichten, sowie Levelup Nachricht bei VoiceXP.',
+  BYE_ID: 'Channel für Verlassensnachrichten von Usern.',
+  LOG_ID:
+    'Channel für Logging des Bots bei Fehlern, sowie bei löschen und editieren von nachrichten, beitritt und verlassen von voice und der vergabe von Rollen.',
+  BUMP_ID: 'Channel für den Bump Reminder',
+  QUIZ_ID: 'Channel für tägliche Quizzes und Quiz Statistik am Wochenende.',
+  ADMIN_C_ID:
+    'Channel für private Feedback Nachrichten von Nutzern sowie die Kontrolle von gekaufen Items und Quizfragen.',
+  KI_BOT: 'Rolle auf die die KI Reagieren soll',
+  SPIELE_ID: 'Channel für Spiel Befehle und Galgenmännchen.',
+  VCCREATION_ID: 'Voicechannel bei dessen beitritt ein neuer erstellt wird.',
+  ADMIN_ROLE_ID:
+    'Rolle die gepingt wird, wenn ein Nutzer bei der Rollenwahl angibt unter 18 zu sein.',
+  MITGLIED_ROLE_ID:
+    'Mitglieder Rolle welche Nutzern beim erreichen von Level 1 vergeben wird.',
+  NEWMEMBER_ROLE_ID: 'Rolle die Nutzern bei Beitritt des Servers gegeben wird.',
+  OBERSTER_PLATZ_ID:
+    'Rolle des Obersten Platz in der Nutzer Liste. Über Shop kaufbar.',
+};
+
+module.exports = { idUses };
