dfe9357b425dcfacdc992fb7481b3fd77b974e9e
CatALoony_ <mc.huenchen321@gmail.com>
2026-07-23 15:12:10 +0200

    dummheit fixen

--- src/events/clientReady/02console-log.js
+++ src/events/clientReady/02console-log.js
@@ -6,8 +6,8 @@ module.exports = {
     console.log(`Logged in as ${client.user.tag}!`);
     try {
       let targetUser = undefined;
-      let message = `Bot ist gestartet.\nDer Bot ist laut API auf ${guilds.size} Server(n):\n`;
       const guilds = await client.guilds.fetch();
+      let message = `Bot ist gestartet.\nDer Bot ist laut API auf ${guilds.size} Server(n):\n`;
       guilds.forEach(async (guild) => {
         if (!targetUser) {
           targetUser = await guild.members.fetch(process.env.ADMIN_ID);
