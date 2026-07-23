a4b8f6f10897053e207a7eac60e295719380950d
CatALoony_ <mc.huenchen321@gmail.com>
2026-07-23 15:24:34 +0200

    fix

--- src/events/clientReady/02console-log.js
+++ src/events/clientReady/02console-log.js
@@ -8,15 +8,13 @@ module.exports = {
       let targetUser = undefined;
       const guilds = await client.guilds.cache;
       let message = `Bot ist gestartet.\nDer Bot ist laut API auf ${guilds.size} Server(n):\n`;
-      guilds.forEach(async (guild) => {
+      await guilds.forEach(async (guild) => {
         if (!targetUser) {
           targetUser = await guild.members.fetch(process.env.ADMIN_ID);
         }
-        console.log(targetUser);
         message = `${message}- ${guild.name} (ID: ${guild.id})`;
       });
       if (targetUser) {
-        console.log(targetUser);
         targetUser.send(message);
       }
     } catch (error) {
